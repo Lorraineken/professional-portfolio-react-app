2a767bd3f16815998bed0a8f042b3c4b1d2969c3
Lorraineken <lorrainekupa@gmail.com>
2023-03-05 11:10:33 +0300

    Added state variables for project form imput

--- src/components/ProjectForm.js
+++ src/components/ProjectForm.js
@@ -1,15 +1,22 @@
-import React from "react";
+import React,{useState} from "react";
 
 function ProjectForm(){
 
+    const [projName,setProjName] = useState("")
+    const [description,setDescription] = useState("")
+
+    function handleSubmit(e){
+        e.preventDefault()
+    }
+
     return (
          
-         <form className="card mt-5"  id="add-details">
+         <form className="card mt-5"  id="add-details" onSubmit={(e)=>handleSubmit(e)}>
           <h4 className="card-header">Add New Project</h4>
           <label htmlFor="name">Name: </label>
-          <input type="text" name="name" id="new-name" />
-          <label htmlFor="description">Description </label>
-          <input type="text" name="Instrument" id="new-project"  />
+          <input type="text" name="name" id="new-name" onChange={(e) => setProjName(e.target.value)}/>
+          <label htmlFor="description">Description: </label>
+          <input type="text" name="Instrument" id="new-project" onChange={(e) => setDescription(e.target.value)} />
           <input type="submit" value="Add Project" />
          </form>
          
