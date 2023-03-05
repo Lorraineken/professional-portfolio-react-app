28c086fc411017709e1c52c2dc0b83de7ee9d756
Lorraineken <lorrainekupa@gmail.com>
2023-03-05 11:02:09 +0300

    Added form for adding a new project

--- src/components/ProjectForm.js
+++ src/components/ProjectForm.js
@@ -3,9 +3,16 @@ import React from "react";
 function ProjectForm(){
 
     return (
-        <div>
-
-        </div>
+         
+         <form className="card mt-5"  id="add-details">
+          <h4 className="card-header">Add New Project</h4>
+          <label htmlFor="name">Name: </label>
+          <input type="text" name="name" id="new-name" />
+          <label htmlFor="description">Description </label>
+          <input type="text" name="Instrument" id="new-project"  />
+          <input type="submit" value="Add Project" />
+         </form>
+         
     )
 }
 
